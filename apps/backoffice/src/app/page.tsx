5f3610d0977ad9d3eb2dddfe767156c36128eaad
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2024-07-16 10:28:39 -0400

    footer created

--- apps/backoffice/src/app/page.tsx
+++ apps/backoffice/src/app/page.tsx
@@ -14,6 +14,7 @@ import Gallery from "@/components/landing/Gallery";
 import Collage from "@/components/landing/Gallery";
 import Testimonials from "@/components/landing/Testimonials";
 import Slide from "@/components/landing/Slide";
+import Footer from "@/components/landing/Footer";
 
 const integrations = [
   {
@@ -128,6 +129,7 @@ export default function Home() {
       />
       <Testimonials />
       <Slide />
+      <Footer />
     </>
   );
 }
